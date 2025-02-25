7e3fe29b3b12af287f8803b690e28d47b177ba49
fikri1205 <150352278+fikri1205@users.noreply.github.com>
2025-02-25 14:49:04 +0700

    update autoslide

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 // ini Javascripts External
+
+let index = 0;
+function autoSlide() {
+    const slider = document.getElementById("slider");
+    index++;
+    if (index > 2) {
+        index = 0;
+    }
+    slider.style.transform = `translateX(-${index * 100}%)`;
+}
+setInterval(autoSlide, 3000);
