55b8cdfe27421ebe7f9048a9647eddeea19afc03
fikri1205 <150352278+fikri1205@users.noreply.github.com>
2025-02-27 07:51:30 +0700

    update full fiture

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,43 @@
-// ini Javascripts External
+// Ini Javascript External
 
-        let index = 0;
-        function autoSlide() {
-            const slider = document.getElementById("slider");
-            index++;
-            if (index > 2) {
-                index = 0;
-            }
-            slider.style.transform = `translateX(-${index * 100}%)`;
-        }
-        setInterval(autoSlide, 3000);
+function validateForm() {
+    let nameInput = document.getElementById('name-input');
+
+    // Kondisi Validasinya
+    if (nameInput.value == '') {
+        alert('Nama tidak boleh kosong');
+    } else {
+        alert('Welcome ' + nameInput.value);
+    }
+}
+
+// For Index Identifier
+let indexBanner = 0;
+
+showBanner();
+
+function nextBanner() {
+    // Increment IndexBanner
+    indexBanner += 1;
+    showBanner();
+}
+
+function showBanner() {
+    let banner = document.getElementsByClassName('main-banner');
+
+    // Reset Condition
+    if (indexBanner > banner.length - 1) {
+        indexBanner = 0;
+    }
+
+    // Loop untuk hide semua
+    for (let i = 0; i < banner.length; i++) {
+        banner[i].style.display = 'none';
+    }
+
+    // Show Banner
+    banner[indexBanner].style.display = 'block';
+}
+
+// Auto Play
+setInterval(nextBanner, 3000);
